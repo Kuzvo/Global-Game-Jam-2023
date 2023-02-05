12607bdbd3c440c3865edd15668502388b21f9ff
broanite <lmcint218@caledonian.ac.uk>
2023-02-05 10:28:48 +0000

    Main Menu Added

--- Global Game Jam/Assets/Scripts/MainMenu.cs
+++ Global Game Jam/Assets/Scripts/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,9 +11,42 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void Settings()
+    public void SetQualityLevel(int index)
     {
-        Debug.Log("wow this works so well!! nice job lewis");
+        Debug.Log("QUALITY ADJUSTED");
+        QualitySettings.SetQualityLevel(index, true);
+        if (index == 3)
+            Time.fixedDeltaTime = 0.002f;
+        else
+            Time.fixedDeltaTime = 0.02f;
+    }
+
+    public void SetVFXVolume(float value)
+    {
+        // Set Value
+        Debug.Log("VFX ADJUSTED");
+    }
+    public void SetMusicVolume(float value)
+    {
+        // Set Value
+        Debug.Log("MUSIC ADJUSTED");
+    }
+
+    public void ChangeScreenSettings(int index)
+    {
+        Debug.Log("DISPLAY ADJUSTED");
+        if (index == 0)
+        {
+            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+        }
+        else if (index == 1)
+        {
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+        }
+        else
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
     }
 
     public void Quit()
